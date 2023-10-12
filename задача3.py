2a3efb375c2d227a5b033b5dfe9f161a25fcb649
Tiredelephant18 <147646766+Tiredelephant18@users.noreply.github.com>
2023-10-12 22:11:22 +0300

    Add files via upload

---
+++ задача3.py
@@ -0,0 +1,5 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+middle_player = int(len(list_players)/2)
+print(list_players[:middle_player])
+print(list_players[middle_player:])
+# TODO Разделите участников на две команды
